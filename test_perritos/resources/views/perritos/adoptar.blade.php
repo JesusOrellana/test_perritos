aee24a3a8c07680b0d84cc5f7ec9fac0344f582e
Jesus Orellana <orellana.jesus1994@gmail.com>
2021-03-28 19:09:22 -0300

    agragando archivo env

--- test_perritos/resources/views/perritos/adoptar.blade.php
+++ test_perritos/resources/views/perritos/adoptar.blade.php
@@ -4,4 +4,5 @@
 <h1>Felicidades has adoptado a {{$perro[0]->nombre}}</h1>
 <h2><i class="fas fa-paw" style="color:red "></i></h2>
 <h2>Te saluda con su patita</h2>
+<a href="{{url('/')}}" class = "btn btn-primary">Volver</a>
 @endsection
